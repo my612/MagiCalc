fd1ace270a527b03568174269d22f9f9d0a326c2
Muhammad Yasser Muhammad Elghazaly <muhammadyasser@aucegypt.edu>
2025-01-08 05:42:36 +0200

    Scripting for all buttons done, A functioning Calculator

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
-let i1, i2;
+setColor = (color, id) => {
+  console.log(color);
+  document.querySelector(`#${id}`).style.backgroundColor = color;
+};
 
-const inp1 = document.getElementById("inp-1");
-const inp2 = document.getElementById("inp-2");
+let currentResult = "";
 
-inp1.addEventListener("input", (event) => {
-  i1 = parseInt(inp1.value, 10);
-});
+const buttons = document.querySelectorAll("button");
 
-inp2.addEventListener("input", (event) => {
-  i2 = parseInt(inp2.value, 10);
-});
+const resultField = document.querySelector('input[name="output"]');
 
-const add = (a, b) => a + b;
-const sub = (a, b) => a - b;
-const mul = (a, b) => a * b;
-const div = (a, b) => a / b;
+const clear = () => {
+  resultField.value = "";
+};
 
 const setResult = (result) => {
-  document.querySelector('input[name="output"]').value = result;
+  resultField.value = result;
+};
+const par = () => {
+  const exp = resultField.value;
+  const result = -1 * eval(exp);
+  setResult(result);
+};
+const equal = () => {
+  const exp = resultField.value;
+  const result = eval(exp);
+  setResult(result);
+};
+const buttonMapping = {
+  clear: clear,
+  "bracket-left": "(",
+  "bracket-right": ")",
+  div: "/",
+  times: "*",
+  sub: "-",
+  add: "+",
+  equal: equal,
+  par: par,
+};
+for (let i = 0; i < 10; i++) {
+  buttonMapping[`N-${i}`] = i;
+}
+
+const handleButton = (button) => {
+  const resultValue = resultField.value;
+  const buttonValue = buttonMapping[`${button.id}`];
+  if (typeof buttonValue === "function") buttonValue();
+  else setResult(`${resultValue}${buttonValue}`);
 };
 
-document.querySelector("#add").onclick = () => {
-  const result = add(i1, i2);
-  if (isNaN(result)) console.log("Error: Please Enter a number");
-  else setResult(result);
-};
-
-document.querySelector("#sub").onclick = () => {
-  const result = sub(i1, i2);
-  if (isNaN(result)) console.log("Error: Please Enter a number");
-  else setResult(result);
-};
-
-document.querySelector("#mul").onclick = () => {
-  const result = mul(i1, i2);
-  if (isNaN(result)) console.log("Error: Please Enter a number");
-  else setResult(result);
-};
-
-document.querySelector("#divi").onclick = () => {
-  const result = div(i1, i2);
-  if (isNaN(result)) console.log("Error: Please Enter a number");
-  else if (result === Infinity) console.log("Undefined Operation");
-  else setResult(result);
-};
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => handleButton(button));
+});
